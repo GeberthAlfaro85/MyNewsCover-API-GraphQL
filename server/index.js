d07f060e689ef2f59feffb4e5857aef328840ecd
Josué Soto <deivysoto66@gmail.com>
2021-08-23 17:58:24 -0600

    Features added

--- server/index.js
+++ server/index.js
@@ -6,6 +6,73 @@ const db = mongoose.connect("mongodb://127.0.0.1:27017/Ultimate-project");
 
 const News = require("./models/newsModel");
 
+/**
+ * Remove the session from the sessionstorage 
+ * and redirect the user to login
+ */
+ function removeSession(){
+  sessionStorage.removeItem("usuarioActivo");
+    window.location.href = "./index.html";
+}
+
+/**
+ * Render sources by userID
+ * @param {*} sources 
+ */
+function renderSources(sources) {
+  let url = "http://localhost:3000/api/categories";
+  const ajaxRequest = new XMLHttpRequest();
+  ajaxRequest.addEventListener("load", (response) => {
+    const taskResponse = JSON.parse(response.target.responseText);
+    let html = `<table class="table table-responsive table-bordered">
+        <tr>
+          <th>Name</th>
+          <th>Category</th>
+          <th>Actions</th>
+        </tr>`;
+    sources.forEach((source) => {
+      taskResponse.forEach((categories) => {
+        if (source.userID === usuario) {
+          if (source.categoryID === categories._id) {
+            html += `<thead class="thead-dark">
+            </thead>
+            <tbody>
+              <tr>
+                <td>${source.nameSource}</td>
+                <td id="cat">${categories.nameCategory}</td>
+                <td><a href="editnews.html?id=${source._id}&idCategory=${source.categoryID}">
+                <button type="button" class="btn btn-success">Edit</button></a>
+                <button onclick="deletesource('${source._id}')" type="button" class="btn btn-danger">Delete</button>
+                </td>
+              </tr>
+            </tbody>`;
+          }
+        } else {
+        }
+      });
+    });
+    html += "</table>";
+    document.getElementById("tableList").innerHTML = html;
+  });
+
+  ajaxRequest.addEventListener("error", error);
+  ajaxRequest.open("GET", url);
+  ajaxRequest.setRequestHeader("Content-Type", "application/json");
+  ajaxRequest.send();
+}
+
+/**
+ * Delete sources by ID
+ * @param {*} id 
+ */
+function deletesource(id) {
+  const ajaxRequest = new XMLHttpRequest();
+  ajaxRequest.addEventListener("error", error);
+  ajaxRequest.open("DELETE", `http://localhost:3000/api/newsSources?id=${id}`);
+  ajaxRequest.send();
+  location.reload();
+}
+
 // functions
 
 //Gets all news 
@@ -97,5 +164,3 @@ app.use('/graphql', graphqlHTTP({
 }));
 
 app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
-
-
